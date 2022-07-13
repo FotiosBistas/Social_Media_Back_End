7d736216f572f2a20ce7c9d290179f7a28f63589
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-07-13 14:46:31 +0300

    Workspaces
    Organized the project into the server workspace and the client workspace

--- Client/src/main.rs
+++ Client/src/main.rs
@@ -1,11 +1,10 @@
 mod profile; 
-mod KDclient; 
-
+mod KDclient;  
 
 fn main() {
     let prof = profile::Profile::new("Fotis","1234");
     println!("{}",prof);
-    KDclient::operations::login(&prof)
+    KDclient::operations::login(&prof); 
 }
 
 
