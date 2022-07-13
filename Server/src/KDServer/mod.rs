ecfce2e555474f15ef44f179802f6b0455ee9d0a
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-07-13 15:48:10 +0300

    Some server implementations

---
+++ Server/src/KDServer/mod.rs
@@ -0,0 +1,17 @@
+
+use crate::Server::server_struct; 
+
+pub mod operations {
+    
+    use super::*; 
+
+    pub fn search(server: &Server){
+
+    }
+
+    pub fn update_directories(server: &Server){
+
+    }
+
+
+} 
--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,3 +1,10 @@
+use std::{net::TcpListener, io::Error};
+
 fn main(){
-    
+
+    //handle error 
+    let tcp_listener = TcpListener::bind("127.0.0.1:178"); 
+
+
+
 }
