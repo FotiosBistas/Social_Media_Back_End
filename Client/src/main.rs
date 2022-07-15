de1cf68afb00affa6e711b2ad63259a2fe3646b5
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-07-16 00:48:12 +0300

    created a basic menu for login and signup

--- Client/src/main.rs
+++ Client/src/main.rs
@@ -1,11 +1,41 @@
-
+use std::io;
+mod config;
 mod profile;
 mod kd_client;
 
 fn main() {
-    let prof = profile::Profile::new("Fotis","1234");
-    println!("{}",prof);
-    kd_client::operations::login(&prof);
+
+
+    loop{
+        println!("Options:");
+
+        println!("0.Exit");
+        println!("1.Sign up");
+        println!("2.Log in");
+
+        let mut option = String::new();
+
+        io::stdin().read_line(&mut option).unwrap();
+        let option:u8 = match option.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("You gave an erroneous input");
+                continue;
+            }
+        };
+
+        match option {
+            1 => {
+                kd_client::operations::signup(&prof);
+            },
+            2 => {
+                kd_client::operations::login(&prof);
+            },
+            _ | 0 => {break;}
+
+        }
+
+    }
 }
 
 
