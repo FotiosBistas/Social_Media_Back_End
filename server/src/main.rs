abb0f60ee260856dc371266d849f5005d0b0bea3
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-07-15 16:09:57 +0300

    initialized the thread pool with 8 threads and give the closure as a parameter

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,23 +2,31 @@ mod thread_pool;
 mod kd_server;
 mod server_struct;
 
-use std::{net::TcpListener, io::Error};
+use std::{net::TcpListener, io::Error, thread};
+use crate::thread_pool::ThreadPool;
+use crate::kd_server::operations;
+
 
 fn main(){
 
+
+    let pool = ThreadPool::new(8);
+
+
     //handle error 
     let tcp_listener = match TcpListener::bind("127.0.0.1:178"){
-        Ok(listener) => Ok(listener),
+        Ok(listener) => listener,
         Err(error) => {
             panic!("Could not bind tcp listener: {}",error);
         }
     };
 
-    let tcp_listener = tcp_listener.unwrap().incoming();
+    let tcp_listener = tcp_listener.incoming();
 
     for stream in tcp_listener{
-        stream.expect("failed");
+        let stream = stream.expect("failed");
 
+        pool.execute(|| {operations::handle_connection(stream)});
 
     }
 
