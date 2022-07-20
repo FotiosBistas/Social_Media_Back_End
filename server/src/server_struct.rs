fc8b2643aaffc0bbfd72386f38307d8d7240bd1a
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-07-21 01:34:05 +0300

    added a mutex for the active connections -> server catalog

--- server/src/server_struct.rs
+++ server/src/server_struct.rs
@@ -1,3 +1,27 @@
+
+use std::sync::{Arc,Mutex};
+
 pub struct Server{
-    
+    catalog :Arc<Mutex<Vec<(String,String)>>>
+}
+
+
+impl Server {
+    pub fn new() -> Server{
+        Server{ catalog: Arc::new(Mutex::new(Vec::with_capacity(10)))}
+    }
+
+    pub fn add_new_catalog_entry(&mut self,tuple: (String,String)){
+        let mut entry = self.catalog.lock().unwrap();
+
+
+        let mut catalog = &mut *entry;
+        //check if catalog contains the entry
+        for i in catalog.iter(){
+            if *i.0 == tuple.0 && *i.1 == tuple.1{
+                return;
+            }
+        }
+        catalog.push(tuple);
+    }
 }
