bc8e8910e30a1e259a9146148d07b5a641060227
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-07-25 19:04:15 +0300

    First try with tokio

--- async_server/src/main.rs
+++ async_server/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    
+use tokio::{net::TcpListener, io::{AsyncReadExt, AsyncWriteExt}};
+
+#[tokio::main]
+async fn main() {
+    let listener = match TcpListener::bind("localhost:8080").await{
+        Ok(listener) =>listener,
+        Err(e) => panic!("Could not open tcp stream: {}",e),
+    };
+
+    let (mut socket,mut addr) = match listener.accept().await{
+        Ok((socket,addr)) => (socket,addr),
+        Err(e) => panic!("Could not accept connection: {}",e)
+    }; 
+
+    loop{
+        let mut buffer = [0u8;1024]; 
+
+        let bytes_read =  socket.read(&mut buffer).await.unwrap(); 
+
+        socket.write_all(&buffer[..bytes_read]).await.unwrap(); 
+    }
 }
